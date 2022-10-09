bf241b25d51471d0479a1be248dc41a18cec5ac1
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-10-09 17:21:38 +0800

    注释修改

--- PP-TinyPose人体姿态识别/PicoDet.cs
+++ PP-TinyPose人体姿态识别/PicoDet.cs
@@ -10,17 +10,23 @@ namespace OpenVinoSharpPPTinyPose
 {
     internal class PicoDet
     {
-        private Core predictor = null;
-        private string input_node_name = "image";
-        private string output_node_name_1 = "concat_8.tmp_0";
-        private string output_node_name_2 = "transpose_8.tmp_0";
-        private Size input_size = new Size(0,0);
-        private int output_length = 0;
+        // 成员变量
+        private Core predictor; // 模型推理器
+        private string input_node_name = "image"; // 模型输入节点名称
+        private string output_node_name_1 = "concat_8.tmp_0"; // 模型预测框输出节点名
+        private string output_node_name_2 = "transpose_8.tmp_0"; // 模型预测置信值输出节点
+        private Size input_size = new Size(0,0); // 模型输入节点形状
+        private int output_length = 0; // 模型输出数据长度
 
         public PicoDet(string mode_path, string device_name) {
             predictor = new Core(mode_path, device_name);
         }
 
+        /// <summary>
+        /// 设置模型输入输出形状
+        /// </summary>
+        /// <param name="input_image_size">输入形状</param>
+        /// <param name="output_length">输出尺寸</param>
         public void set_shape(Size input_image_size, int output_length) 
         { 
             this.input_size = input_image_size;
@@ -31,8 +37,6 @@ namespace OpenVinoSharpPPTinyPose
         public List<Rect> predict(Mat image)
         {
             // 设置图片输入
-            // 配置图片数据            
-            // 将图片放在矩形背景下
             // 图片数据解码
             byte[] input_image_data = image.ImEncode(".bmp");
             // 数据长度
@@ -40,6 +44,10 @@ namespace OpenVinoSharpPPTinyPose
             // 设置图片输入
             predictor.load_input_data(input_node_name, input_image_data, input_image_length, 0);
 
+            // 求取缩放大小
+            double scale_x = (double)image.Width / (double)this.input_size.Width;
+            double scale_y = (double)image.Height / (double)this.input_size.Height;
+            Point2d scale_factor = new Point2d(scale_x, scale_y);
             // 模型推理
             predictor.infer();
 
@@ -49,20 +57,22 @@ namespace OpenVinoSharpPPTinyPose
             float[] results_con = predictor.read_infer_result<float>(output_node_name_2, output_length);
             // 读取预测框
             float[] result_box = predictor.read_infer_result<float>(output_node_name_1, 4 * output_length);
+            // 处理模型推理数据
+            List<Rect> boxes_result = process_result(results_con, result_box, scale_factor);
+            return boxes_result;
+        }
 
-
-            // 求取缩放大小
-            double scale_x = (double)image.Width / (double)this.input_size.Width;
-            double scale_y = (double)image.Height / (double)this.input_size.Height;
+        private List<Rect> process_result(float[] results_con, float[] result_box, Point2d scale_factor)
+        {
 
             // 处理预测结果
             List<float> confidences = new List<float>();
             List<Rect> boxes = new List<Rect>();
             for (int c = 0; c < output_length; c++)
             {
-                Rect rect = new Rect((int)(result_box[4 * c] * scale_x), (int)(result_box[4 * c + 1] * scale_y),
-                    (int)((result_box[4 * c + 2] - result_box[4 * c]) * scale_x),
-                    (int)((result_box[4 * c + 3] - result_box[4 * c + 1]) * scale_y));
+                Rect rect = new Rect((int)(result_box[4 * c] * scale_factor.X), (int)(result_box[4 * c + 1] * scale_factor.Y),
+                    (int)((result_box[4 * c + 2] - result_box[4 * c]) * scale_factor.X),
+                    (int)((result_box[4 * c + 3] - result_box[4 * c + 1]) * scale_factor.Y));
                 boxes.Add(rect);
                 confidences.Add(results_con[c]);
             }
@@ -70,7 +80,7 @@ namespace OpenVinoSharpPPTinyPose
             int[] indexes = new int[boxes.Count];
             CvDnn.NMSBoxes(boxes, confidences, 0.5f, 0.5f, out indexes);
             List<Rect> boxes_result = new List<Rect>();
-            for (int i = 0; i < indexes.Length; i++) 
+            for (int i = 0; i < indexes.Length; i++)
             {
                 boxes_result.Add(boxes[indexes[i]]);
             }
