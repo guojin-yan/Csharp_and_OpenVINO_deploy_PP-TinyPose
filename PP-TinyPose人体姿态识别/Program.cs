63c352bc7d87feda35e95953ef876e8268baaeac
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-10-11 19:07:46 +0800

    代码修改

--- PP-TinyPose人体姿态识别/Program.cs
+++ PP-TinyPose人体姿态识别/Program.cs
@@ -7,7 +7,8 @@ namespace OpenVinoSharpPPTinyPose
     {
         static void Main(string[] args)
         {
-            tiny_pose_image();
+            //tiny_pose_image();
+            Time_text.test_time();
         }
 
         public static void tiny_pose_image() 
